6c0f3a29708dc272b6d7c7c8f30f83423712d7f2
CenJieTeng <2271369683@qq.com>
2025-10-20 02:34:45 +0800

    添加资源表格视图插件

--- Scripts/UnitData.gd
+++ Scripts/UnitData.gd
@@ -1,7 +1,7 @@
-class_name UnitData extends RefCounted
-var stats : Dictionary
-var exp : int
-var level : int
+class_name UnitData extends Resource
+@export var stats : Dictionary
+@export var exp : int
+@export var level : int
 
 func _init(p_stats: Dictionary):
 	stats = p_stats
